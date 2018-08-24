8dc60abfe9a9293f5b66433b8efa130a1c81f595
Bryce Klinker <bklinker@pillartechnology.com>
2018-08-23 20:44:37 -0500

    Moved code into countries service

--- server/tests/Lively.Football.Tests/CountriesTests.cs
+++ server/tests/Lively.Football.Tests/CountriesTests.cs
@@ -10,10 +10,20 @@ namespace Lively.Football.Tests
         [Fact]
         public async Task GivenNoCountries_WhenCountriesAreLoaded_ThenCountriesAreLoadedFromDataSource()
         {
+            var service = new CountriesService();
+            var response = await service.LoadAll();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+
+    public class CountriesService
+    {
+        public async Task<HttpResponseMessage> LoadAll()
+        {
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync("https://apifootball.com/api/?action=get_countries&APIkey=f0bd2551475a981323ba7a83eeea738e96820b9206e5a6f51503af4d5b375c63");
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                return response;
             }
         }
     }
