43fe6a274560d5986897b8297e5375d8f36dc375
Bryce Klinker <bklinker@pillartechnology.com>
2018-10-06 20:17:17 -0500

    Removing all code to restart using blazor

--- server/src/Lively.Football.Application/Common/IDataSourceConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Lively.Football.Application.Common
-{
-    public interface IDataSourceConfig
-    {
-        string ApiKey { get; }
-        string BaseUrl { get; }
-    }
-}
--- server/src/Lively.Football.Application/Countries/Entities/Country.cs
+++
@@ -1,9 +0,0 @@
-namespace Lively.Football.Application.Countries.Entities
-{
-    public class Country
-    {
-        public long Id { get; set; }
-        public string SourceId { get; set; }
-        public string Name { get; set; }
-    }
-}
